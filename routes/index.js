904f50de92c15973c4495fb24345bc20cbe38844
Sabrina Koumoin <sabrina.koumoin@gmail.com>
2019-12-16 15:39:46 -0500

    Creating route getAnimalsAndBreeds

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+var animals = [{
+  type:"dog",
+  breeds:["Golden Retriever","German Sheperd","French Poodle"]
+},
+{
+  type:"cat",
+  breeds:["French Cat","Aristocat"]
+},
+{
+  type:"hamster",
+  breeds:["Hamster1","Hamster2"]
+}]
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/getAnimalsAndBreeds",function(req,res){
+    res.json(animals)
+})
+
 
 router.get("/test",function(req,res){
   res.json({name:"Sabrina"})
