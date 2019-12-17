a0898c3d986ad1b32788fd7382a6bc677295c7d1
Sabrina Koumoin <sabrina.koumoin@gmail.com>
2019-12-16 23:31:32 -0500

    adding more dogs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var animalsAvailable = [{
   type:"dog",
-  breeds:["Golden Retriever","German Shepherd","French Poodle"]
+  breeds:["Golden Retriever","German Shepherd","Poodle", "Pitbull"]
 },
 {
   type:"cat", 
@@ -20,19 +20,89 @@ var animalsToAdopt = [
     name:"Jodie",
     type:"dog",
     breed:"Golden Retriever",
-    image:"https://adopt-me-spa.s3.amazonaws.com/black-puppy.jpg",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/golden-retriever1.jpg",
     zipCode:22207,
-    age:12
+    age:7
 
   },
   {
-    id:2,
-    name:"Jonah",
+    id:1,
+    name:"Johnny",
+    type:"dog",
+    breed:"Golden Retriever",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/german-shepherd2.jpg",
+    zipCode:22201,
+    age:15
+
+  },
+  {
+    id:3,
+    name:"Luca",
+    type:"dog",
+    breed:"Pitbull",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/pitbull1.jpg",
+    zipCode:95051,
+    age:13
+
+  },
+  {
+    id:4,
+    name:"Luca",
+    type:"dog",
+    breed:"Pitbull",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/pitbull1.jpg",
+    zipCode:95051,
+    age:20
+
+  },
+  {
+    id:5,
+    name:"Moses",
+    type:"dog",
+    breed:"Pitbull",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/pitbull2.jpg",
+    zipCode:95051,
+    age:7
+
+  },
+  {
+    id:6,
+    name:"Touffy",
+    type:"dog",
+    breed:"Poodle",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/poodle1.jpeg",
+    zipCode:23457,
+    age:8
+
+  },
+  {
+    id:7,
+    name:"Boule",
+    type:"dog",
+    breed:"Poodle",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/poodle2.jpg",
+    zipCode:21117,
+    age:9
+
+  },
+  {
+    id:8,
+    name:"King",
     type:"dog",
     breed:"German Shepherd",
-    image:"https://adopt-me-spa.s3.amazonaws.com/domestic-dog_thumb.jpg",
-    zipCode:22000,
-    age:40
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/german-shepherd1.jpg",
+    zipCode:24444,
+    age:5
+
+  },
+  {
+    id:8,
+    name:"Queen",
+    type:"dog",
+    breed:"German Shepherd",
+    image:"https://adopt-me-spa.s3.amazonaws.com/The+Pets/german-shepherd2.jpg",
+    zipCode:24444,
+    age:5
 
   }
 ]
