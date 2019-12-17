4f5739df06e7840927d0e8330fa7f15816cf457d
Sabrina Koumoin <sabrina.koumoin@gmail.com>
2019-12-16 20:04:59 -0500

    fixing

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,7 @@ router.post("/findAnimals",function(req,res){
   var results = []
   for(var i=0;i<animalsToAdopt.length;i++){
       var item = animalsToAdopt[i]
-      if( (item.type === req.body.animalType) && (item.breed === req.body.animalBreed) && (item.zipCode === req.body.zipCode) && (req.body.maxAge <= item.age) ){
+      if( (item.type === req.body.animalType) && (item.breed === req.body.animalBreed) && (item.zipCode === req.body.zipCode) && (req.body.maxAge < item.age || req.body.maxAge === item.age) ){
         results.push(item)
         
       }
